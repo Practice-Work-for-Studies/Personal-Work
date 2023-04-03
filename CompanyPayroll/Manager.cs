17cbba3b030bdb9462e980625e12e3256404f20d
AlexGiannakopoulos <117894640+AlexGiannakopoulos@users.noreply.github.com>
2023-04-03 19:33:58 +0300

    Add files via upload

---
+++ CompanyPayroll/Manager.cs
@@ -0,0 +1,16 @@
+﻿namespace CompanyPayroll;
+
+public class Manager : Member
+{
+    public Manager(string Name) : base(Name)
+    {
+        this.Category = "Manager";
+        
+    }
+
+    public override double calcSalary()
+    {
+        this.Salary = 2500;
+        return Salary;
+    }
+}
